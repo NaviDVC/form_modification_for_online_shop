63fe0122a4d164ea877edd0fc074505ec6228ae9
NaviDVC <ivan999az.vf@gmail.com>
2023-07-11 03:10:18 +0300

    showForm func

--- main.js
+++ main.js
@@ -1,71 +1,84 @@
-let choice = document.querySelectorAll('.item');
-let carsList = document.querySelector('.middle__cars-list');
-let phonesList = document.querySelector('.middle__phones-list');
-let buyButton = document.querySelectorAll('.middle__choose-item');
-let bmw = document.querySelector('.right__bmw');
-let ford = document.querySelector('.right__ford');
-let iphone = document.querySelector('.right__iphone');
-let nokia = document.querySelector('.right__nokia');
+const choice = document.querySelectorAll(".item");
+const carsList = document.querySelector(".middle__cars-list");
+const phonesList = document.querySelector(".middle__phones-list");
+const buyButton = document.querySelectorAll(".middle__choose-item");
+const bmw = document.querySelector(".right__bmw");
+const ford = document.querySelector(".right__ford");
+const iphone = document.querySelector(".right__iphone");
+const nokia = document.querySelector(".right__nokia");
+const buttons = document.querySelectorAll(".buy");
+const confirmButton = document.getElementById("confirm");
 
 function cars() {
-  carsList.style.display = 'block';
-  phonesList.style.display = 'none';
+  carsList.style.display = "block";
+  phonesList.style.display = "none";
   console.log(buyButton);
 }
 
 function phones() {
-  carsList.style.display = 'none';
-  phonesList.style.display = 'block';
+  carsList.style.display = "none";
+  phonesList.style.display = "block";
   console.log(buyButton);
 }
 
 function bmwCar() {
-    bmw.style.display = 'block';
-    ford.style.display = 'none';
-    iphone.style.display = 'none';
-    nokia.style.display = 'none';
-    window.name = 'BMW M8 Competition';
+  bmw.style.display = "block";
+  ford.style.display = "none";
+  iphone.style.display = "none";
+  nokia.style.display = "none";
+  window.name = "BMW M8 Competition";
 }
 
 function fordCar() {
-    bmw.style.display = 'none';
-    ford.style.display = 'block';
-    iphone.style.display = 'none';
-    nokia.style.display = 'none';
-    window.name = 'Ford Mustang Shelby GT500';
+  bmw.style.display = "none";
+  ford.style.display = "block";
+  iphone.style.display = "none";
+  nokia.style.display = "none";
+  window.name = "Ford Mustang Shelby GT500";
 }
 
 function iphoPhone() {
-    bmw.style.display = 'none';
-    ford.style.display = 'none';
-    iphone.style.display = 'block';
-    nokia.style.display = 'none';
-    window.name = 'iPhone 11 Pro Max 256GB';
+  bmw.style.display = "none";
+  ford.style.display = "none";
+  iphone.style.display = "block";
+  nokia.style.display = "none";
+  window.name = "iPhone 11 Pro Max 256GB";
 }
 
 function nokiaPhone() {
-    bmw.style.display = 'none';
-    ford.style.display = 'none';
-    iphone.style.display = 'none';
-    nokia.style.display = 'block';
-    window.name = 'Nokia 3310 0MB';
+  bmw.style.display = "none";
+  ford.style.display = "none";
+  iphone.style.display = "none";
+  nokia.style.display = "block";
+  window.name = "Nokia 3310 0MB";
 }
 
-choice[0].addEventListener('click', cars);
 
-choice[1].addEventListener('click', phones);
+choice[0].addEventListener("click", cars);
 
-buyButton[0].addEventListener('click', bmwCar);
+choice[1].addEventListener("click", phones);
 
-buyButton[1].addEventListener('click', fordCar);
+buyButton[0].addEventListener("click", bmwCar);
 
-buyButton[2].addEventListener('click', iphoPhone);
+buyButton[1].addEventListener("click", fordCar);
 
-buyButton[3].addEventListener('click', nokiaPhone);
+buyButton[2].addEventListener("click", iphoPhone);
 
-document.addEventListener('click', (event) => {
-    if (event.target.dataset.buy !== undefined) {
-        window.message = alert(`You bought ${name}!`)
-        location.reload()
-    }
-});
+buyButton[3].addEventListener("click", nokiaPhone);
+
+function showForm() {
+  let buyForm = document.querySelector(".buy_form");
+  buyForm.style.display = "block";
+}
+
+for (let button of buttons) {
+  button.addEventListener("click", (event) => {
+    // if (event.target.dataset.buy !== undefined) {
+    //     window.message = alert(`You bought ${name}!`)
+    //     location.reload()
+    // }
+
+    showForm();
+  });
+}
+
